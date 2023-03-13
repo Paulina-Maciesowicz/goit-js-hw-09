479a90d2c72cf18da01e80cfd14ec73bd98f0225
Paulina-Maciesowicz <paulinamaa@gmail.com>
2023-03-13 19:22:27 +0100

    update

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,6 +1,8 @@
 const date = new Date();
 const jsBtn = document.querySelector(".js-btn");
 jsBtn.disabled = true;
+let chooseTime = null;
+let timerId = null;
 
 const options = {
   enableTime: true,
@@ -11,8 +13,63 @@ const options = {
     console.log(selectedDates[0]);
     if (selectedDates[0] < date) {
       window.alert("Please choose a date in the future");
+      jsBtn.disabled = true;
+    } else {
+      jsBtn.disabled = false;
+      chooseTime = selectedDates[0];
     }
   },
 };
 
 flatpickr(document.querySelector("#datetime-picker"), options);
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+function counter() {
+  const date = new Date();
+  const timeObj = convertMs(chooseTime - date);
+  document.querySelector(".value-days").innerHTML = addLeadingZero(
+    timeObj.days
+  );
+  document.querySelector(".value-hours").innerHTML = addLeadingZero(
+    timeObj.hours
+  );
+  document.querySelector(".value-minutes").innerHTML = addLeadingZero(
+    timeObj.minutes
+  );
+  document.querySelector(".value-seconds").innerHTML = addLeadingZero(
+    timeObj.seconds
+  );
+}
+
+jsBtn.addEventListener("click", () => {
+  timerId = setInterval(() => {
+    jsBtn.disabled = true;
+    counter();
+  }, 1000);
+});
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, "0");
+}
