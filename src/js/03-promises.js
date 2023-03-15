f78e31fd72347414087985d3130b18b9eb7f66d2
Paulina-Maciesowicz <paulinamaa@gmail.com>
2023-03-15 20:33:38 +0100

    update

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,34 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+const form = document.querySelector(".form");
+
+function createPromise(number, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ number, delay });
+      } else {
+        reject({ number, delay });
+      }
+    }, delay);
+  });
 }
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const {
+    elements: { delay, step, amount },
+  } = event.currentTarget;
+  console.log(delay.value, step.value, amount.value);
+  for (let i = 1; i <= amount.value; i++) {
+    const convertNumberDelay = parseInt(delay.value);
+    const convertNumberStep = parseInt(step.value);
+    const promiseDelay = convertNumberDelay + convertNumberStep * (i-1);
+    createPromise(i, promiseDelay)
+      .then(({ number, delay }) => {
+        console.log(`✅ Fulfilled promise ${number} in ${delay}ms`);
+      })
+      .catch(({ number, delay }) => {
+        console.log(`❌ Rejected promise ${number} in ${delay}ms`);
+      });
+  }
+});
